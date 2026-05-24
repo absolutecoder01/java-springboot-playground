8557e95c5415b5a29afd9d531664fa0a87b01784
Volodymyr <absolute.coder01@gmail.com>
2026-05-24 19:14:06 +0200

    Authenticating a User with LDAP

---
+++ gs-authenticating-ldap/src/test/java/com/absolutecoder01/authenticating_ldap/AuthenticatingLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.absolutecoder01.authenticating_ldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticatingLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
