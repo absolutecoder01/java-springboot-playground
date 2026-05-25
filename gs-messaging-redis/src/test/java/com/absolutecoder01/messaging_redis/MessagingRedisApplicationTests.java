340e4017a08d3094e9e40f14e257a6ef380e5314
Volodymyr <absolute.coder01@gmail.com>
2026-05-25 10:58:51 +0200

    Messaging with Redis

---
+++ gs-messaging-redis/src/test/java/com/absolutecoder01/messaging_redis/MessagingRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.absolutecoder01.messaging_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
