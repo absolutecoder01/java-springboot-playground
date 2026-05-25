06ba99820e09f7cf16a495a04edefc3755b9b561
Volodymyr <absolute.coder01@gmail.com>
2026-05-25 11:52:13 +0200

    Messaging with RabbitMQ

---
+++ gs-messaging-rabbitmq/src/test/java/com/absolutecoder01/messaging_rabbitmq/MessagingRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.absolutecoder01.messaging_rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
